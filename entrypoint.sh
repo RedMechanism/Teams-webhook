c27d539cf9fb07d568b88d05a6ab71529b1250cc
RedMechanism <temrob@yahoo.com>
2020-08-14 17:59:50 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 
-export webhook=$1
-export message=${@:2}
-
-cat << EOF > webhook.py
-#!/usr/bin/python3
-import pymsteams
-import sys
-webhook = sys.argv[1]
-message = ' '.join(sys.argv[2:])
-myTeamsMessage = pymsteams.connectorcard(webhook)
-myTeamsMessage.text(message)
-myTeamsMessage.send()
-EOF
-
-chmod 755 webhook.py
-
-./webhook.py $webhook $message
+echo $which-team
+python3 /app/send_message.py
