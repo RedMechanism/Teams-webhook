1aacf00ce294a9d844f59a4c4127d011d2987b64
RedMechanism <temrob@yahoo.com>
2020-08-18 15:18:34 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ cat << EOF > ms_teams_notification.py
 import pymsteams
 import os
 from datetime import datetime, timedelta
-# You must create the connectorcard object with the Microsoft Webhook URL
+
+# You must create the connector card object with the Microsoft Webhook URL
 WEB_HOOK_URL = os.getenv('webhook_url', None)
 REPOSITORY_NAME = os.getenv('repository_name', None)
 REPOSITORY_URL = os.getenv('repository_url', None)
@@ -16,9 +17,13 @@ JOB_NAME = os.getenv('job_name', None)
 ACTOR = os.getenv('actor', None)
 PR_NUMBER = os.getenv('pr_number', None)
 RUN_ID = os.getenv('run_id', None)
+RELEASE_TAG = os.getenv('release_tag', None)
+
 if WEB_HOOK_URL is None:
     print("Please set WEB_HOOK_URL env")
     exit(1)
+
+# Status colour definitions
 myTeamsMessage = pymsteams.connectorcard(WEB_HOOK_URL)
 color = None
 if JOB_STATUS.lower() == 'failure':
@@ -29,25 +34,38 @@ elif JOB_STATUS.lower() == 'success':
     color = 'green'
 else:
     color = 'blue'
+
+# Subsection elelments
 myMessageSection = pymsteams.cardsection()
 myTeamsMessage.title(f'''{REPOSITORY_NAME}''')
 myTeamsMessage.addSection(myMessageSection)
+
 # Activity Elements
-myMessageSection.activityTitle("On branch: "+BRANCH)
+if RELEASE_TAG is None:
+    myMessageSection.activityTitle("On branch: "+BRANCH)
+else:
+    myMessageSection.activityTitle("Release: "+RELEASE_TAG)
 myMessageSection.activitySubtitle("Triggered by: "+ACTOR)
 myMessageSection.activityImage("https://github.com/"+ACTOR+".png")
 JST_time = datetime.now() + timedelta(hours = 9)
 myMessageSection.activityText(JST_time.strftime("%Y-%m-%d %H:%M:%S"))
+
 # Facts are key value pairs displayed in a list.
 myMessageSection.addFact("Status:", "<strong style='color: {textcolor}';>{Result}</strong>".format(textcolor = color, Result = JOB_STATUS.upper()))
 myMessageSection.addFact("Job:", JOB_NAME)
 myMessageSection.addFact("Event:", EVENT)
-# Add text to the message.
+
+# Add a text element. It is empty but we need it for proper functionality
 myTeamsMessage.text(f'''
 ''')
 myTeamsMessage.color("Red")
+
+# Links
 myTeamsMessage.addLinkButton("Go to workflow", "https://github.com/"+REPOSITORY_URL+"/actions/runs/"+RUN_ID+"?check_suite_focus=true")
-myTeamsMessage.addLinkButton("Go to pull request", "https://github.com/"+REPOSITORY_URL+"/pulls/"+PR_NUMBER)
+if RELEASE_TAG is None:
+    myTeamsMessage.addLinkButton("Go to pull request", "https://github.com/"+REPOSITORY_URL+"/pull/"+PR_NUMBER)
+else:
+    myTeamsMessage.addLinkButton("Go to release", "https://github.com/"+REPOSITORY_URL+"/releases/tag/"+RELEASE_TAG)
 myTeamsMessage.send()
 EOF
 
